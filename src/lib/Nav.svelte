ecc281fbcfdda329fe629d7fd71f6cd1141d5ceb
isaeff007 <isaeff007@gmail.com>
2021-11-26 15:21:36 +0100

    - add Navigation (style with bulma)

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,56 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="/img/sk.png"alt='logo'>
+        </a>
+    
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbar" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href='/components'>
+            Components
+          </a>
+    
+          <a class="navbar-item" href='/users'>
+            Users
+          </a>
+    
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+    
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+</nav>
+
+
+ <style>
+     img {
+         object-fit: cover;
+         width: 3rem;
+     }
+ </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Nav from '$lib/Nav.svelte';
 	import '../app.scss';
 </script>
 
+<Nav />
 <slot />
